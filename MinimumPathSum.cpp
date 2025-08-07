3116da9012559cbb28125ce2dd91696ed528cc45
Suchit Gourkhede <87932869+suchit-10@users.noreply.github.com>
2025-08-07 19:00:03 +0000

    Now it's ok.

--- MinimumPathSum.cpp
+++ MinimumPathSum.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        // Start position
-        if (obstacleGrid[0][0] == 1) return 0;
-        dp[0][0] = 1;
-        // First column
-        for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0] == 0)
-                dp[i][0] = dp[i-1][0];
-        }
+        dp[0][0] = grid[0][0];
+
         // First row
-        for (int j = 1; j < n; j++) {
-            if (obstacleGrid[0][j] == 0)
-                dp[0][j] = dp[0][j-1];
+        for (int j = 1; j < m; ++j)
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+
+        // First column
+        for (int i = 1; i < n; ++i)
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+
         // Rest of the grid
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 0)
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        return dp[m-1][n-1];
+
+        return dp[n - 1][m - 1];
     }
 };
